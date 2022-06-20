69f740b0c9563cd7b92114c8231084c9d376db97
TriDC <docaotri@1997@gmail.com>
2022-06-20 15:42:24 +0700

    update

--- shared/aws/lambda/index.js
+++ shared/aws/lambda/index.js
@@ -3,7 +3,7 @@ const AWS = require('aws-sdk');
 module.exports.invoke = ({ body, functionName, invokeType = 'Event' }) => {
     const lambda = new AWS.Lambda({
         region: 'ap-southeast-1',
-       //  endpoint: process.env.IS_OFFLINE ? 'http://localhost:3002' : undefined,
+        endpoint: process.env.IS_OFFLINE ? 'http://localhost:3002' : undefined,
     });
     const params = {
         FunctionName: functionName,
