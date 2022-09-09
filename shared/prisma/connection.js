b7505b765d3cfde0fd4dace70b03dc3e057201d7
TriDC <docaotri@1997@gmail.com>
2022-09-09 13:22:27 +0700

    update

--- shared/prisma/connection.js
+++ shared/prisma/connection.js
@@ -1,6 +1,5 @@
-const { PrismaClient } = require('@prisma/client');
-
-let prisma = new PrismaClient();
+const { PrismaClient } = require('@prisma/client/edge')
+const prisma = new PrismaClient();
 
 module.exports.createConnection = () => {
     return prisma;
