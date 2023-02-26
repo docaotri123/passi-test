98597d6193d9d5ee567d36a88d470160001b92b9
tridc@innotech-vn.com <tridc@innotech-vn.com>
2023-02-26 09:43:51 +0700

    update ci

--- src/service/v1/seed.ts
+++ src/service/v1/seed.ts
@@ -4,12 +4,13 @@ const fs = require('fs');
 export default class SeedService {
     public async create() {
         const seedDir = path.join(__dirname, '../../../seeds');
+        console.log('seed')
     
         const filenames = fs.readdirSync(seedDir);
         const _filenames = filenames.sort();
     
         for (const filename of _filenames) {
-            if (filename !== 'index.js' && filename.slice(-3) === '.js') {
+            if (filename !== 'index.ts' && filename.slice(-3) === '.ts') {
                 const seedFile = require(path.join(seedDir, filename));
     
                 if (seedFile.seed) {
